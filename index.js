2a5282165a3049f541f5d2577af9aedce25ddf9f
Anthony Urbina <aurbinava002@gmail.com>
2023-02-07 05:08:09 +0000

    server verifies message signatures

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ const server = http.createServer(app);
 const path = require('node:path');
 const publicPath = path.join(__dirname, 'dist');
 const staticMiddleware = express.static(publicPath);
-// const siwe = require('siwe');
+const siwe = require('siwe');
 app.use(staticMiddleware);
-
-// const domain = 'localhost';
-// const origin = 'https://localhost:5500/login';
-
-// function createSiweMessage(address, statement) {
-//   const siweMessage = new siwe.SiweMessage({
-//     domain,
-//     address,
-//     statement,
-//     uri: origin,
-//     version: '1',
-//     chainId: '1'
-//   });
-//   return siweMessage.prepareMessage();
-// }
-
-// console.log(createSiweMessage('0x07c233C36ac7103bDDD8fdebE9935fE871BF5474', 'Anthony wants you to sign in with your Ethereum account'));
+app.use(express.json());
+app.get('/nonce', function (req, res) {
+  const nonce = siwe.generateNonce();
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(nonce);
+});
+const { SiweMessage } = siwe;
+app.post('/verify', async function (req, res) {
+  const { message, signature } = req.body;
+  const siweMessage = new SiweMessage(message);
+  try {
+    await siweMessage.validate(signature);
+    res.send(true);
+  } catch (err) {
+    console.error(err);
+    res.send(false);
+  }
+});
 
 server.listen(3000, () => {
   // eslint-disable-next-line no-console
--- src/index.js
+++ src/index.js
@@ -3,39 +3,61 @@ import { ethers } from 'ethers';
 
 const $connectWalletBtn = document.querySelector('#connect-wallet-btn');
 const $siweBtn = document.querySelector('#siwe-btn');
-// $connectWalletBtn.onClick = connectWallet;
-// $siweBtn.onClick = signInWithEthereum;
+const $verifyBtn = document.querySelector('#verify-btn');
 
 $connectWalletBtn.addEventListener('click', connectWallet);
 $siweBtn.addEventListener('click', signInWithEthereum);
+$verifyBtn.addEventListener('click', sendForVerification);
 
 const domain = window.location.host;
 const origin = window.location.origin;
 const provider = new ethers.providers.Web3Provider(window.ethereum);
+
 const signer = provider.getSigner();
 
-function createSiweMessage(address, statement) {
-  const siweMessage = new SiweMessage({
+async function createSiweMessage(address, statement) {
+  const res = await fetch('/nonce');
+  const message = new SiweMessage({
     domain,
     address,
     statement,
     uri: origin,
     version: '1',
-    chainId: '1'
+    chainId: '1',
+    nonce: await res.text()
   });
-  return siweMessage.prepareMessage();
+  return message.prepareMessage();
 }
 
 function connectWallet() {
-  // console.log('byw');
   provider.send('eth_requestAccounts', [])
+    // eslint-disable-next-line no-console
+    .then(address => console.log(address))
     .catch(err => console.error(err));
 }
 
+let signature;
+let message;
+
 async function signInWithEthereum() {
-  const message = createSiweMessage(await signer.getAddress(), 'Sign in with Ethereum to access App.');
+  try {
+    message = await createSiweMessage(await signer.getAddress(), 'Sign in with Ethereum to access App.');
+    signature = await signer.signMessage(message);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function sendForVerification() {
+  const res = await fetch('/verify', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ message, signature })
+  });
   // eslint-disable-next-line no-console
-  console.log('signature:', await signer.signMessage(message));
+  console.log(await res.text());
 }
 
 // console.log(createSiweMessage('0x07c233C36ac7103bDDD8fdebE9935fE871BF5474', 'Anthony wants you to sign in with your Ethereum account'));

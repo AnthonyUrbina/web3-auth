359d23698b11d01322815aa31ecb73e354137211
Anthony Urbina <aurbinava002@gmail.com>
2023-02-10 00:32:43 +0000

    red text when user tries to enter area 51 with no authorized access

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import { ethers } from 'ethers';
 import jwtDecode from 'jwt-decode';
 
 const $connectWalletBtn = document.querySelector('#connect-wallet-btn');
-const $siweBtn = document.querySelector('#siwe-btn');
-const $verifyBtn = document.querySelector('#verify-btn');
+// const $siweBtn = document.querySelector('#siwe-btn');
+// const $verifyBtn = document.querySelector('#verify-btn');
 const $area51Btn = document.querySelector('#area51-btn');
+const $header = document.querySelector('#header');
+const $siweBtn = document.createElement('button');
+const $area51Txt = document.createElement('p');
+const $area51Container = document.querySelector('.area51-container');
 
-$connectWalletBtn.addEventListener('click', connectWallet);
 $siweBtn.addEventListener('click', signInWithEthereum);
-$verifyBtn.addEventListener('click', sendForVerification);
+$connectWalletBtn.addEventListener('click', connectWallet);
+// $verifyBtn.addEventListener('click', sendForVerification);
 $area51Btn.addEventListener('click', enterArea51);
+
+$siweBtn.textContent = 'Sign In';
+$siweBtn.id = 'siwe-btn';
 
 const domain = window.location.host;
 const origin = window.location.origin;
@@ -39,27 +46,47 @@ async function createSiweMessage(address, statement) {
   return message.prepareMessage();
 }
 
-function connectWallet() {
+function changeButton(event, address) {
+  if (event.target.id === 'connect-wallet-btn') {
+    $connectWalletBtn.remove();
+    $header.appendChild($siweBtn);
+  }
+
+  if (event.target.id === 'siwe-btn') {
+    $siweBtn.remove();
+    const $signOut = document.createElement('button');
+    $signOut.textContent = address;
+    $signOut.id = 'siwe-btn';
+    $header.appendChild($signOut);
+  }
+}
+
+function connectWallet(event) {
   provider.send('eth_requestAccounts', [])
     // eslint-disable-next-line no-console
-    .then(address => console.log(address))
+    .then(address => {
+      if (!address.error) {
+        changeButton(event);
+      }
+    })
     .catch(err => console.error('err swagg', err));
 }
 
 let signature;
 let message;
 
-async function signInWithEthereum() {
+async function signInWithEthereum(event) {
   try {
     const address = await signer.getAddress();
     message = await createSiweMessage(address, 'Sign in with Ethereum to access App.');
     signature = await signer.signMessage(message);
-
+    changeButton(event, address);
   } catch (err) {
     console.error(err);
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 async function sendForVerification() {
   const token = window.localStorage.getItem('auth-jwt');
 
@@ -79,13 +106,21 @@ async function sendForVerification() {
 }
 
 async function enterArea51() {
-  const _token = window.localStorage.getItem('siwe-jwt');
-  const headers = {
-    'Content-Type': 'application/json',
-    'X-Access-Token': _token
-  };
-  const res = await fetch('/area51', { headers });
-  const message = await res.json();
-  // eslint-disable-next-line no-console
-  console.log(message);
+  try {
+    const _token = window.localStorage.getItem('siwe-jwt');
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-Access-Token': _token
+    };
+    const res = await fetch('/area51', { headers });
+    const message = await res.json();
+
+    // eslint-disable-next-line no-console
+    console.log(message);
+  } catch (err) {
+    console.error(err);
+    $area51Txt.textContent = 'You do not have access!';
+    $area51Txt.className = 'red';
+    $area51Container.appendChild($area51Txt);
+  }
 }
